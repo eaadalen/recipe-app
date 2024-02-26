6dc15a2ab348f6c09558360a29fa926ff707c577
eaadalen <eaadalen112@gmail.com>
2024-02-25 20:33:01 -0600

    2.5 updates

--- RAVE/src/recipes/models.py
+++ RAVE/src/recipes/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.shortcuts import reverse
 
 # Create your models here.
 class Recipe(models.Model):
@@ -9,4 +10,25 @@ class Recipe(models.Model):
     pic = models.ImageField(upload_to='recipes', default='no_picture.jpg')
 
     def __str__(self):
-        return str(self.name)
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse ("recipes:detail", kwargs={"pk": self.pk})
+    
+    def return_ingredients_as_list(self):
+        return self.ingredients.split(",")
+    
+    def calculate_difficulty(self):
+        num_ingredients = len(self.return_ingredients_as_list())
+        if self.cooking_time < 10 and num_ingredients < 4:
+            return "Easy"
+        elif self.cooking_time < 10 and num_ingredients >= 4:
+            return "Medium"
+        elif self.cooking_time >= 10 and num_ingredients < 4:
+            return "Intermediate"
+        elif self.cooking_time >= 10 and num_ingredients >= 4:
+            return "Hard"
+        
+    def save(self, *args, **kwargs):
+        self.difficulty = self.calculate_difficulty()
+        super().save(*args, **kwargs)
--- RAVE/src/recipes/views.py
+++ RAVE/src/recipes/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView   #to display lists
+from .models import Recipe                #to access Recipe model
 
 # Create your views here.
 def home(request):
    return render(request, '../templates/recipes/recipes_home.html')
+
+# Create your views here.
+class RecipeListView(ListView):           #class-based view
+   model = Recipe                         #specify model
+   template_name = '../templates/recipes/recipe_list.html'    #specify template 
+
+class RecipeDetailView(DetailView):
+    model = Recipe
+    template_name = "../templates/recipes/recipe_details.html"
